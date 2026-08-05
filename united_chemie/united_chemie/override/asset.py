9f18b99063d22af91e51790623745714a2846500
solufyin <online@DESKTOP-TCNAS06.localdomain>
2026-08-05 09:39:10 +0000

    enabled commented code

--- united_chemie/united_chemie/override/asset.py
+++ united_chemie/united_chemie/override/asset.py
@@ -1,9 +1,10 @@
 import frappe
-from erpnext.assets.doctype.asset.asset import get_straight_line_or_manual_depr_amount
-from erpnext.assets.doctype.asset.asset import get_shift_depr_amount
-from erpnext.assets.doctype.asset.asset import get_wdv_or_dd_depr_amount
+from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import get_straight_line_or_manual_depr_amount
+from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import get_shift_depr_amount
+from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import get_wdv_or_dd_depr_amount
 
 def custom_get_depreciation_amount(
+    asset_depr_schedule,
 	asset,
 	depreciable_value,
 	yearly_opening_wdv,
@@ -12,16 +13,17 @@ def custom_get_depreciation_amount(
 	prev_depreciation_amount=0,
 	has_wdv_or_dd_non_yearly_pro_rata=False,
 	number_of_pending_depreciations=0,
+	prev_per_day_depr= 0,
 ):
 	frappe.flags.company = asset.company
 
 	if fb_row.depreciation_method in ("Straight Line", "Manual"):
 		return get_straight_line_or_manual_depr_amount(
-			asset, fb_row, schedule_idx, number_of_pending_depreciations
-		)
+			asset_depr_schedule, asset, fb_row, schedule_idx, number_of_pending_depreciations
+		), None
 	else:
 		if fb_row.shift_based and fb_row.depreciation_method == "Written Down Value":
-			return get_shift_depr_amount(asset, fb_row, schedule_idx)
+			return get_shift_depr_amount(asset_depr_schedule, asset, fb_row, schedule_idx), None
 
 		return get_wdv_or_dd_depr_amount(
 			asset,
@@ -31,4 +33,6 @@ def custom_get_depreciation_amount(
 			schedule_idx,
 			prev_depreciation_amount,
 			has_wdv_or_dd_non_yearly_pro_rata,
+			asset_depr_schedule,
+			prev_per_day_depr,
 		)
